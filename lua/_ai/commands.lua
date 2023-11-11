ae46624ab947afe3e2bb2a0938763f8a9e289b84
Christopher Allène <christopher.allene@mobsuccess.com>
2023-11-11 08:26:35 +0000

    Fix support of gp4, broke indent

--- lua/_ai/commands.lua
+++ lua/_ai/commands.lua
@@ -1,148 +1,176 @@
 local M = {}
 
 local openai = require("_ai/openai")
-local config = require('_ai/config')
+local config = require("_ai/config")
 
 local ns_id = vim.api.nvim_create_namespace("")
 
 ---@param args { args: string, range: integer }
-function M.ai (args)
-    local prompt = args.args
-    local visual_mode = args.range > 0
+function M.ai(args)
+	local prompt = args.args
+	local visual_mode = args.range > 0
 
-    local buffer = vim.api.nvim_get_current_buf()
+	local buffer = vim.api.nvim_get_current_buf()
 
-    local start_row, start_col
-    local end_row, end_col
+	local start_row, start_col
+	local end_row, end_col
 
-    if visual_mode then
-        -- Use the visual selection
-        local start_pos = vim.api.nvim_buf_get_mark(buffer, "<")
-        start_row = start_pos[1] - 1
-        start_col = start_pos[2]
+	if visual_mode then
+		-- Use the visual selection
+		local start_pos = vim.api.nvim_buf_get_mark(buffer, "<")
+		start_row = start_pos[1] - 1
+		start_col = start_pos[2]
 
-        local end_pos = vim.api.nvim_buf_get_mark(buffer, ">")
-        end_row = end_pos[1] - 1
-        local line = vim.fn.getline(end_pos[1])
-        if line == "" then
-            end_col = 0
-        else
-            end_col = vim.fn.byteidx(line, vim.fn.charcol("'>"))
-        end
+		local end_pos = vim.api.nvim_buf_get_mark(buffer, ">")
+		end_row = end_pos[1] - 1
+		local line = vim.fn.getline(end_pos[1])
+		if line == "" then
+			end_col = 0
+		else
+			end_col = vim.fn.byteidx(line, vim.fn.charcol("'>"))
+		end
+	else
+		-- Use the cursor position
+		local start_pos = vim.api.nvim_win_get_cursor(0)
+		start_row = start_pos[1] - 1
+		local line = vim.fn.getline(start_pos[1])
+		if line == "" then
+			start_col = 0
+		else
+			start_col = vim.fn.byteidx(line, vim.fn.charcol("."))
+		end
+		end_row = start_row
+		end_col = start_col
+	end
 
-    else
-        -- Use the cursor position
-        local start_pos = vim.api.nvim_win_get_cursor(0)
-        start_row = start_pos[1] - 1
-        local line = vim.fn.getline(start_pos[1])
-        if line == "" then
-            start_col = 0
-        else
-            start_col = vim.fn.byteidx(line, vim.fn.charcol("."))
-        end
-        end_row = start_row
-        end_col = start_col
-    end
+	local start_line_length = vim.api.nvim_buf_get_lines(buffer, start_row, start_row + 1, true)[1]:len()
+	start_col = math.min(start_col, start_line_length)
 
-    local start_line_length = vim.api.nvim_buf_get_lines(buffer, start_row, start_row+1, true)[1]:len()
-    start_col = math.min(start_col, start_line_length)
+	local end_line_length = vim.api.nvim_buf_get_lines(buffer, end_row, end_row + 1, true)[1]:len()
+	end_col = math.min(end_col, end_line_length)
 
-    local end_line_length = vim.api.nvim_buf_get_lines(buffer, end_row, end_row+1, true)[1]:len()
-    end_col = math.min(end_col, end_line_length)
+	local extmark_opts = {
+		end_row = end_row,
+		end_col = end_col,
+		hl_group = "AIHighlight",
+	}
+	if config.indicator_style == "sign" then
+		extmark_opts.sign_text = config.indicator_text
+		extmark_opts.sign_hl_group = "AIIndicator"
+	elseif config.indicator_style == "virtual_text" then
+		extmark_opts.virt_text = { { config.indicator_text, "AIIndicator" } }
+	elseif config.indicator_style ~= "none" then
+		vim.api.nvim_err_writeln("ai.vim: Unsupported value for g:ai_indicator_style")
+		return
+	end
 
-    local extmark_opts = {
-        end_row = end_row,
-        end_col = end_col,
-        hl_group = "AIHighlight",
-    }
-    if config.indicator_style == "sign" then
-        extmark_opts.sign_text = config.indicator_text
-        extmark_opts.sign_hl_group = "AIIndicator"
-    elseif config.indicator_style == "virtual_text" then
-        extmark_opts.virt_text = {{ config.indicator_text, "AIIndicator" }}
-    elseif config.indicator_style ~= "none" then
-        vim.api.nvim_err_writeln("ai.vim: Unsupported value for g:ai_indicator_style")
-        return
-    end
+	local mark_id = vim.api.nvim_buf_set_extmark(buffer, ns_id, start_row, start_col, extmark_opts)
 
-    local mark_id = vim.api.nvim_buf_set_extmark(buffer, ns_id, start_row, start_col, extmark_opts)
+	local function on_result(err, result)
+		local mark = vim.api.nvim_buf_get_extmark_by_id(buffer, ns_id, mark_id, { details = true })
+		local start_row = mark[1]
+		local start_col = mark[2]
+		local end_row = mark[3].end_row
+		local end_col = mark[3].end_col
 
-    local function on_result (err, result)
-        local mark = vim.api.nvim_buf_get_extmark_by_id(buffer, ns_id, mark_id, { details = true })
-        local start_row = mark[1]
-        local start_col = mark[2]
-        local end_row = mark[3].end_row
-        local end_col = mark[3].end_col
+		print("err", vim.inspect(err))
+		print("result", vim.inspect(result))
 
-        vim.api.nvim_buf_del_extmark(buffer, ns_id, mark_id)
+		vim.api.nvim_buf_del_extmark(buffer, ns_id, mark_id)
 
-        if err then
-            vim.api.nvim_err_writeln("ai.vim: " .. err)
-        else
-            local text = result.choices[1].text
-            if result.model and string.find(result.model, "gpt-3.5-turbo", 1, true) then
-                text = result.choices[1].message.content
-            end
-            local lines = {}
-            for line in text:gmatch("([^\n]*)\n?") do
-                table.insert(lines, line)
-            end
+		if err then
+			vim.api.nvim_err_writeln("ai.vim: " .. err)
+		else
+			local text = result.choices[1].text
+			if
+				result.model
+				and (string.find(result.model, "gpt-3.5-turbo", 1, true) or string.find(result.model, "gpt-4", 1, true))
+			then
+				text = result.choices[1].message.content
+			end
+			local lines = {}
+			if text then
+				for line in text:gmatch("([^\n]*)\n?") do
+					table.insert(lines, line)
+				end
+			end
 
-            -- -- Special case: prepend \n if we're dealing with a multi-line response
-            -- if #lines > 1 then
-            --     table.insert(lines, 1, "")
-            -- end
+			-- -- Special case: prepend \n if we're dealing with a multi-line response
+			-- if #lines > 1 then
+			--     table.insert(lines, 1, "")
+			-- end
 
-            vim.api.nvim_buf_set_text(buffer, start_row, start_col, end_row, end_col, lines)
-        end
-    end
+			vim.api.nvim_buf_set_text(buffer, start_row, start_col, end_row, end_col, lines)
+		end
+	end
 
-    if visual_mode then
-        local selected_text = table.concat(vim.api.nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col, {}), "\n")
-        if prompt == "" then
-            -- Replace the selected text, also using it as a prompt
-            openai.call("completions", {
-                model = config.completions_model,
-                prompt = selected_text,
-                max_tokens = 2048,
-                temperature = config.temperature,
-            }, on_result)
-        else
-            -- Edit selected text
-            openai.call("edits", {
-                model = config.edits_model,
-                input = selected_text,
-                instruction = prompt,
-                temperature = config.temperature,
-            }, on_result)
-        end
-    else
-        if prompt == "" then
-            -- Insert some text generated using surrounding context
-            local prefix = table.concat(vim.api.nvim_buf_get_text(buffer,
-                math.max(0, start_row-config.context_before), 0, start_row, start_col, {}), "\n")
+	if visual_mode then
+		local selected_text = table.concat(
+			vim.api.nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col, {}),
+			"\n"
+		)
+		if prompt == "" then
+			-- Replace the selected text, also using it as a prompt
+			openai.call("completions", {
+				model = config.completions_model,
+				prompt = selected_text,
+				max_tokens = 2048,
+				temperature = config.temperature,
+			}, on_result)
+		else
+			-- Edit selected text
+			openai.call("edits", {
+				model = config.edits_model,
+				input = selected_text,
+				instruction = prompt,
+				temperature = config.temperature,
+			}, on_result)
+		end
+	else
+		if prompt == "" then
+			-- Insert some text generated using surrounding context
+			local prefix = table.concat(
+				vim.api.nvim_buf_get_text(
+					buffer,
+					math.max(0, start_row - config.context_before),
+					0,
+					start_row,
+					start_col,
+					{}
+				),
+				"\n"
+			)
 
-            local line_count = vim.api.nvim_buf_line_count(buffer)
-            local suffix = table.concat(vim.api.nvim_buf_get_text(buffer,
-                end_row, end_col, math.min(end_row+config.context_after, line_count-1), 99999999, {}), "\n")
+			local line_count = vim.api.nvim_buf_line_count(buffer)
+			local suffix = table.concat(
+				vim.api.nvim_buf_get_text(
+					buffer,
+					end_row,
+					end_col,
+					math.min(end_row + config.context_after, line_count - 1),
+					99999999,
+					{}
+				),
+				"\n"
+			)
 
-            openai.call("completions", {
-                model = config.completions_model,
-                prompt = prefix,
-                suffix = suffix,
-                max_tokens = 2048,
-                temperature = config.temperature,
-            }, on_result)
-        else
-            -- Insert some text generated using the given prompt
-            openai.call("completions", {
-                model = config.completions_model,
-                prompt = prompt,
-                max_tokens = 2048,
-                temperature = config.temperature,
-            }, on_result)
-        end
-    end
+			openai.call("completions", {
+				model = config.completions_model,
+				prompt = prefix,
+				suffix = suffix,
+				max_tokens = 2048,
+				temperature = config.temperature,
+			}, on_result)
+		else
+			-- Insert some text generated using the given prompt
+			openai.call("completions", {
+				model = config.completions_model,
+				prompt = prompt,
+				max_tokens = 2048,
+				temperature = config.temperature,
+			}, on_result)
+		end
+	end
 end
 
 return M
--- lua/_ai/openai.lua
+++ lua/_ai/openai.lua
@@ -1,98 +1,106 @@
 local M = {}
 
-local config = require('_ai/config')
+local config = require("_ai/config")
 
 ---@param cmd string
 ---@param args string[]
 ---@param on_result fun(err: string?, output: string?)
-local function exec (cmd, args, on_result)
-    local stdout = vim.loop.new_pipe()
-    local stdout_chunks = {}
-    local function on_stdout_read (_, data)
-        if data then
-            table.insert(stdout_chunks, data)
-        end
-    end
+local function exec(cmd, args, on_result)
+	local stdout = vim.loop.new_pipe()
+	local stdout_chunks = {}
+	local function on_stdout_read(_, data)
+		if data then
+			table.insert(stdout_chunks, data)
+		end
+	end
 
-    local stderr = vim.loop.new_pipe()
-    local stderr_chunks = {}
-    local function on_stderr_read (_, data)
-        if data then
-            table.insert(stderr_chunks, data)
-        end
-    end
+	local stderr = vim.loop.new_pipe()
+	local stderr_chunks = {}
+	local function on_stderr_read(_, data)
+		if data then
+			table.insert(stderr_chunks, data)
+		end
+	end
 
-    -- print(cmd, vim.inspect(args))
+	-- print(cmd, vim.inspect(args))
 
-    local handle
+	local handle
 
-    handle, error = vim.loop.spawn(cmd, {
-        args = args,
-        stdio = {nil, stdout, stderr},
-    }, function (code)
-        stdout:close()
-        stderr:close()
-        handle:close()
+	handle, error = vim.loop.spawn(cmd, {
+		args = args,
+		stdio = { nil, stdout, stderr },
+	}, function(code)
+		stdout:close()
+		stderr:close()
+		handle:close()
 
-        vim.schedule(function ()
-            if code ~= 0 then
-                on_result(vim.trim(table.concat(stderr_chunks, "")))
-            else
-                on_result(nil, vim.trim(table.concat(stdout_chunks, "")))
-            end
-        end)
-    end)
+		vim.schedule(function()
+			if code ~= 0 then
+				on_result(vim.trim(table.concat(stderr_chunks, "")))
+			else
+				on_result(nil, vim.trim(table.concat(stdout_chunks, "")))
+			end
+		end)
+	end)
 
-    if not handle then
-        on_result(cmd .. " could not be started: " .. error)
-    else
-        stdout:read_start(on_stdout_read)
-        stderr:read_start(on_stderr_read)
-    end
+	if not handle then
+		on_result(cmd .. " could not be started: " .. error)
+	else
+		stdout:read_start(on_stdout_read)
+		stderr:read_start(on_stderr_read)
+	end
 end
 
 ---@param endpoint string
 ---@param body table
 ---@param on_result fun(err: string?, output: unknown?): nil
-function M.call (endpoint, body, on_result)
-    if body.model == "gpt-3.5-turbo" then
-        endpoint = "chat/completions"
-        body.messages = {
-            [1] = {
-                role = "user",
-                content = body.prompt,
-            },
-        }
-        body.prompt = nil
-        body.suffix = nil
-    end
-    local api_key = os.getenv("OPENAI_API_KEY")
-    if not api_key then
-        on_result("$OPENAI_API_KEY environment variable must be set")
-        return
-    end
+function M.call(endpoint, body, on_result)
+	if true or body.model:find("gpt-4") or body.model == "gpt-3.5-turbo" then
+		endpoint = "chat/completions"
+		body.messages = {
+			[1] = {
+				role = "user",
+				content = body.prompt,
+			},
+		}
+		body.prompt = nil
+		body.suffix = nil
+	end
+	local api_key = os.getenv("OPENAI_API_KEY")
+	if not api_key then
+		on_result("$OPENAI_API_KEY environment variable must be set")
+		return
+	end
 
-    local curl_args = {
-        "-X", "POST", "--silent", "--show-error",
-        "-L", "https://api.openai.com/v1/" .. endpoint,
-        "-m", config.timeout,
-        "-H", "Content-Type: application/json",
-        "-H", "Authorization: Bearer " .. api_key,
-        "-d", vim.json.encode(body),
-    }
+	local curl_args = {
+		"-X",
+		"POST",
+		"--silent",
+		"--show-error",
+		"-L",
+		"https://api.openai.com/v1/" .. endpoint,
+		"-m",
+		config.timeout,
+		"-H",
+		"Content-Type: application/json",
+		"-H",
+		"Authorization: Bearer " .. api_key,
+		"-d",
+		vim.json.encode(body),
+	}
 
-    exec("curl", curl_args, function (err, output)
-        if err then
-            on_result(err)
-        else
-            local json = vim.json.decode(output)
-            if json.error then
-                on_result(json.error.message)
-            else
-                on_result(nil, json)
-            end
-        end
-    end)
+	exec("curl", curl_args, function(err, output)
+		if err then
+			on_result(err)
+		else
+			local json = vim.json.decode(output)
+			if json.error then
+				on_result(json.error.message)
+			else
+				on_result(nil, json)
+			end
+		end
+	end)
 end
 
 return M
